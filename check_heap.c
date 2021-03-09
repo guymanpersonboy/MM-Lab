46a82a8f6c141f5b3c34bfc461b4bf27e8434acb
Christopher Carrasco <chris5@cs.utexas.edu>
2021-03-08 18:22:39 -0600

    update comments

--- check_heap.c
+++ check_heap.c
@@ -19,20 +19,20 @@ int check_heap() {
     while (cur != NULL) {
         char prev_mark = is_allocated(prev);
         char cur_mark = is_allocated(cur);
-        // 1. check if every block in the free list is marked as free
+        // 1. Check if every block in the free list is marked as free
         if (prev_mark || cur_mark) {
             all_marked_free = false;
         } else  {
             // 2. Is every free block on the free list?
             free_blocks_count++;
         }
-        // check address order of prev and cur, and check
-        // 9. are there any contiguous free blocks that somehow escaped coalescing.
+        // 9. Are there any contiguous free blocks that somehow escaped coalescing?
+        // 10. Is the free list in memory order?
         if (check_subsequent_blocks(prev, cur)) {
             return EXIT_FAILURE;
         }
-        // 6. do any allocated blocks overlap with each other?
-        // 7. do any allocated blocks overlap with a free block?
+        // 6. Do any allocated blocks overlap with each other?
+        // 7. Do any allocated blocks overlap with a free block?
         if (check_for_overlap(prev)) {
             return EXIT_FAILURE;
         }
@@ -50,9 +50,8 @@ int check_heap() {
 }
 
 /**
- * Check if subsequent free_blocks in the free list are in
- * increasing order of their address in memory, and
- * they did not escape coalescing.
+ * Check if subsequent free_blocks did not escape coalescing.
+ * Check if if free blocks are in memory order.
  * 
  * @param prev is the previous memory_block_t in the free list.
  * @param cur is the current memory_block_t in the free list.
@@ -60,12 +59,12 @@ int check_heap() {
  */
 static bool check_subsequent_blocks(memory_block_t *prev, memory_block_t *cur) {
     size_t prev_size = get_size(prev);
-    return prev >= cur || prev + prev_size == cur;
+    return prev + prev_size == cur || prev >= cur;
 }
 
 /**
- * Check if any allocated blocks overlap with each other. Check if any allocated
- * blocks overlap with a free block.
+ * Check if any allocated blocks overlap with each other.
+ * Check if any allocated blocks overlap with a free block.
  * 
  * @param prev is the previous memory_block_t in the free list.
  * @param cur is the current memory_block_t in the free list.
